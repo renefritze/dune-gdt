21f300fda168d3ea8c019f087f8b4bcd39ed6611
Rene Milk <rene.milk@wwu.de>
2017-02-16 09:26:49 +0100

    [operators] add more prolongation variations

--- dune/gdt/operators/prolongations.hh
+++ dune/gdt/operators/prolongations.hh
@@ -116,6 +116,33 @@ public:
 
   template< class GPS, int pS, class R, size_t r, size_t rC, class VS, class GPR, int pR, class VR >
   inline void apply(const ConstDiscreteFunction
+      < Spaces::CG::PdelabBased< GPS, pS, R, r, rC >, VS >& source,
+                    DiscreteFunction< Spaces::DG::PdelabBased< GPR, pR, R, r, rC >, VR >&
+                    range) const
+  {
+    prolong_onto_dg_fem_localfunctions_wrapper(source, range);
+  }
+
+  template< class GPS, int pS, class R, size_t r, size_t rC, class VS, class GPR, int pR, class VR >
+  inline void apply(const ConstDiscreteFunction
+      < Spaces::DG::PdelabBased< GPS, pS, R, r, rC >, VS >& source,
+                    DiscreteFunction< Spaces::CG::PdelabBased< GPR, pR, R, r, rC >, VR >&
+                    range) const
+  {
+    prolong_onto_dg_fem_localfunctions_wrapper(source, range);
+  }
+
+  template< class GPS, int pS, class R, size_t r, size_t rC, class VS, class GPR, int pR, class VR >
+  inline void apply(const ConstDiscreteFunction
+      < Spaces::DG::PdelabBased< GPS, pS, R, r, rC >, VS >& source,
+                    DiscreteFunction< Spaces::DG::PdelabBased< GPR, pR, R, r, rC >, VR >&
+                    range) const
+  {
+    prolong_onto_dg_fem_localfunctions_wrapper(source, range);
+  }
+
+  template< class GPS, int pS, class R, size_t r, size_t rC, class VS, class GPR, int pR, class VR >
+  inline void apply(const ConstDiscreteFunction
                       < Spaces::Block< Spaces::DG::FemBased< GPS, pS, R, r, rC > >, VS >& source,
                     DiscreteFunction< Spaces::Block< Spaces::DG::FemBased< GPR, pR, R, r, rC > >, VR >&
                       range) const
@@ -468,6 +495,31 @@ private:
                                                     < GPR, pR, RR, rR, rCR >, VR >& range) const
   {
     lagrange_prolongation_operator_.apply(source, range);
+  }
+
+  template< class GPS, int pS, class RS, size_t rS, size_t rCS, class VS,
+      class GPR, int pR, class RR, size_t rR, size_t rCR, class VR >
+  inline void redirect_to_appropriate_operator(const ConstDiscreteFunction
+      < Spaces::CG::PdelabBased
+            < GPS, pS, RS, rS, rCS >, VS >& source,
+                                               DiscreteFunction
+                                                   < Spaces::DG::PdelabBased
+                                                         < GPR, pR, RR, rR, rCR >, VR >& range) const
+  {
+    l2_prolongation_operator_.apply(source, range);
+  }
+
+
+  template< class GPS, int pS, class RS, size_t rS, size_t rCS, class VS,
+      class GPR, int pR, class RR, size_t rR, size_t rCR, class VR >
+  inline void redirect_to_appropriate_operator(const ConstDiscreteFunction
+      < Spaces::DG::PdelabBased
+            < GPS, pS, RS, rS, rCS >, VS >& source,
+                                               DiscreteFunction
+                                                   < Spaces::DG::PdelabBased
+                                                         < GPR, pR, RR, rR, rCR >, VR >& range) const
+  {
+    l2_prolongation_operator_.apply(source, range);
   }
 
   const L2Prolongation< GridViewType > l2_prolongation_operator_;
