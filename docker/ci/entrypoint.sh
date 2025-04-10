6db027277829ee7b23b83b8c9b7bfd69f5780189
Felix Schindler <107669806+ftschindler@users.noreply.github.com>
2025-04-10 14:39:30 +0200

    docker| bake venv fixing into entrypoint

--- docker/ci/entrypoint.sh
+++ docker/ci/entrypoint.sh
@@ -8,6 +8,13 @@ fi
 export LANG=en_US.UTF-8
 echo "127.0.0.1 ${HOSTNAME}" >> /etc/hosts
 
+export VNEV_SITE_PKG_PATH=$(. /venvs/${OPTS}.sh && python -c "import site; print(site.getsitepackages()[0])")
+if [[ -e "${VNEV_SITE_PKG_PATH}/dune.xt-nspkg.pth" ]] ; then
+    echo '[entrypoint] ensuring lines in dune.*-nspkg.pth are short enough in venv'
+    sed "s/;/\n/g" -i "${VNEV_SITE_PKG_PATH}/dune.xt-nspkg.pth"
+    sed "s/;/\n/g" -i "${VNEV_SITE_PKG_PATH}/dune.gdt-nspkg.pth"
+fi
+
 echo '[entrypoint] use ${DUNECONTROL} to configure and build dune-gdt'
 echo
 
